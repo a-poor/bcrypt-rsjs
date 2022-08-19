ab78f9d70310a9e9b9b41a7afb95b4432c9db814
Austin Poor <45295232+a-poor@users.noreply.github.com>
2022-08-19 13:58:11 -0700

    Adding async versions of hash/compare

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,8 @@ const COST_OUT_OF_RANGE_ERR_MSG: &str = "hash cost out of range [4,32]";
 /// - rounds: uint (optional)
 ///
 /// JS Function Return:
-/// - string 
-fn js_hash(mut cx: FunctionContext) -> JsResult<JsString> {
+/// - string
+fn js_sync_hash(mut cx: FunctionContext) -> JsResult<JsString> {
     // Get the arguments
     let arg_data = cx.argument::<JsString>(0)?;
     let arg_rounds = cx.argument_opt(1);
@@ -24,27 +24,68 @@ fn js_hash(mut cx: FunctionContext) -> JsResult<JsString> {
 
     // Check if a cost was supplied (otherwise uses default)
     if let Some(arg_rounds) = arg_rounds {
+        // Convert from JsValue to JsNumber
         let rounds: Handle<JsNumber> = arg_rounds.downcast_or_throw(&mut cx)?;
+
+        // Get the underlying value
         let n = rounds.value(&mut cx);
 
-        // Check the value
+        // Check the value is in range
         if n < MIN_COST.into() {
             return cx.throw_error(COST_OUT_OF_RANGE_ERR_MSG);
         }
         if n > MAX_COST.into() {
             return cx.throw_error(COST_OUT_OF_RANGE_ERR_MSG);
         }
+
+        // Set the cost
         cost = n as u32;
     }
 
     // Run the hash
-    let h = bcrypt::hash(
-        data, 
-        cost,
-    ).expect("Unable to hash data.");
+    let h = bcrypt::hash(data, cost).expect("Unable to hash data.");
 
     // Return the result
     Ok(cx.string(h))
+}
+
+fn js_async_hash(mut cx: FunctionContext) -> JsResult<JsPromise> {
+    // Get the arguments
+    let arg_data = cx.argument::<JsString>(0)?;
+    let arg_rounds = cx.argument_opt(1);
+
+    // Get the underlying data
+    let data = arg_data.value(&mut cx);
+    let mut cost = DEFAULT_COST;
+
+    // Check if a cost was supplied (otherwise uses default)
+    if let Some(arg_rounds) = arg_rounds {
+        // Convert from JsValue to JsNumber
+        let rounds: Handle<JsNumber> = arg_rounds.downcast_or_throw(&mut cx)?;
+
+        // Get the underlying value
+        let n = rounds.value(&mut cx);
+
+        // Check the value is in range
+        if n < MIN_COST.into() {
+            return cx.throw_error(COST_OUT_OF_RANGE_ERR_MSG);
+        }
+        if n > MAX_COST.into() {
+            return cx.throw_error(COST_OUT_OF_RANGE_ERR_MSG);
+        }
+
+        // Set the cost
+        cost = n as u32;
+    }
+
+    let promise = cx
+        .task(move || bcrypt::hash(data, cost))
+        .promise(move |mut cx, h| {
+            let h = h.expect("Unable to hash data.");
+            Ok(cx.string(h))
+        });
+
+    Ok(promise)
 }
 
 /// JS Function Arguments:
@@ -52,22 +93,41 @@ fn js_hash(mut cx: FunctionContext) -> JsResult<JsString> {
 /// - hash: string
 ///
 /// JS Function Return:
-/// - bool 
-fn js_compare(mut cx: FunctionContext) -> JsResult<JsBoolean> {
+/// - bool
+fn js_sync_compare(mut cx: FunctionContext) -> JsResult<JsBoolean> {
     // Get the arguments
     let data = cx.argument::<JsString>(0)?.value(&mut cx);
     let hash = cx.argument::<JsString>(1)?.value(&mut cx);
 
     // Run the hash
-    let res = bcrypt::verify(data, &hash).expect("Unable to verify data against hash"); // TODO - Change the unwraps to error handling...
+    let res = bcrypt::verify(data, &hash).expect("Error occured verifying data against hash."); // TODO - Change the unwraps to error handling...
 
     // Return the result
     Ok(cx.boolean(res))
 }
 
+fn js_async_compare(mut cx: FunctionContext) -> JsResult<JsPromise> {
+    // Get the arguments
+    let data = cx.argument::<JsString>(0)?.value(&mut cx);
+    let hash = cx.argument::<JsString>(1)?.value(&mut cx);
+
+    // Run the hash
+    let promise = cx
+        .task(move || bcrypt::verify(data, &hash))
+        .promise(move |mut cx, r| {
+            let r = r.expect("Error occured verifying data against hash.");
+            Ok(cx.boolean(r))
+        });
+
+    // Return the async promise
+    Ok(promise)
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hash", js_hash)?;
-    cx.export_function("compare", js_compare)?;
+    cx.export_function("hash", js_sync_hash)?;
+    cx.export_function("compare", js_sync_compare)?;
+    cx.export_function("async_hash", js_async_hash)?;
+    cx.export_function("async_compare", js_async_compare)?;
     Ok(())
 }
